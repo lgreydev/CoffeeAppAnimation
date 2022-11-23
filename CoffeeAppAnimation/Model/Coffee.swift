694bdff225a6a39c98598e0936de53a6007025c8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-22 22:37:25 -0500

    Fix image

---
+++ CoffeeAppAnimation/Model/Coffee.swift
@@ -0,0 +1,20 @@
+//
+//  Coffee.swift
+//  CoffeeAppAnimation
+//
+//  Created by Sergey Lukaschuk on 2022-11-22.
+//
+
+import SwiftUI
+
+struct Coffee {
+    var id: UUID = .init()
+    var imageName: String
+    var title: String
+    var price: String
+}
+
+var coffee: [Coffee] = [
+
+
+]
