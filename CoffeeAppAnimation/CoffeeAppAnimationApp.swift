66a2cf43242a00afd2ae90efdc791c3c8894e1e7
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-22 22:21:11 -0500

    Initial Commit

---
+++ CoffeeAppAnimation/CoffeeAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppAnimationApp.swift
+//  CoffeeAppAnimation
+//
+//  Created by Sergey Lukaschuk on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
