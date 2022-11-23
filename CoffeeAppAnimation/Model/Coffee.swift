b3eb264bd95871d5fdd4c8b8ccee660944062fe9
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-22 22:45:28 -0500

    Add model

--- CoffeeAppAnimation/Model/Coffee.swift
+++ CoffeeAppAnimation/Model/Coffee.swift
@@ -15,6 +15,12 @@ struct Coffee {
 }
 
 var coffee: [Coffee] = [
-
-
+    .init(imageName: "Item 1", title: "Caramel\nCold Drink", price: "$3.90"),
+    .init(imageName: "Item 2", title: "Caramel\nMacchiato", price: "$2.30"),
+    .init(imageName: "Item 3", title: "Iced Coffee\nMocha", price: "$9.20"),
+    .init(imageName: "Item 4", title: "Toffee Nut\nCrunch Latte", price: "$12.30"),
+    .init(imageName: "Item 5", title: "Styled Cold\nCoffee", price: "$8.90"),
+    .init(imageName: "Item 6", title: "Classic Irish\nCoffee", price: "$6.10"),
+    .init(imageName: "Item 7", title: "Black Tea\nLatte", price: "$2.20"),
+    .init(imageName: "Item 8", title: "Iced Coffee\nMocha", price: "$5.90"),
 ]
