2637d260c2b7bd8cc9f4492f96ab27b1e0b668c5
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-23 20:06:16 -0500

    Create home screen

---
+++ CoffeeAppAnimation/View/Home.swift
@@ -0,0 +1,32 @@
+//
+//  Home.swift
+//  CoffeeAppAnimation
+//
+//  Created by Sergey Lukaschuk on 2022-11-23.
+//
+
+import SwiftUI
+
+struct Home: View {
+
+
+
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+
+            VStack(spacing: 0) {
+                ForEach(coffee) { coffee in
+                    // DOTO
+                }
+            }
+        }
+        .preferredColorScheme(.light)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
