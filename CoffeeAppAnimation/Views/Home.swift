db130ecba8d7b817ef5f7b076b1f927baaf9c34f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-24 21:21:08 -0500

    Setup size

--- CoffeeAppAnimation/Views/Home.swift
+++ CoffeeAppAnimation/Views/Home.swift
@@ -11,12 +11,15 @@ struct Home: View {
     var body: some View {
         GeometryReader {
             let size = $0.size
+            let cardSize = size.width
 
             VStack(spacing: 0) {
                 ForEach(Coffee.all) { coffee in
                     CoffeeView(coffee: coffee, size: size)
                 }
             }
+            .frame(width: size.width)
+            .padding(.top, size.height - cardSize)
         }
         .preferredColorScheme(.light)
     }
