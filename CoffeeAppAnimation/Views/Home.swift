44f6828ed33dcce56c2cf431c2bfa07fd9b6b91d
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-27 14:59:39 -0500

    Animation and card scale settings

--- CoffeeAppAnimation/Views/Home.swift
+++ CoffeeAppAnimation/Views/Home.swift
@@ -63,7 +63,7 @@ struct CoffeeView: View {
 
     var body: some View {
         let cardSize = size.width
-        let maxCardsDisplaySize = size.width * 3
+        let maxCardsDisplaySize = size.width * 4
 
         GeometryReader { proxy in
             let _size = proxy.size
@@ -77,6 +77,9 @@ struct CoffeeView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: _size.width, height: _size.height)
                 .scaleEffect(reducedScale < 0 ? 0.001 : reducedScale, anchor: .init(x: 0.5, y: 1 - (currentCardScale / 2)))
+                .scaleEffect(offset > 0 ? 1 + currentCardScale : 1, anchor: .top)
+                .offset(y: offset > 0 ? currentCardScale * 200 : 0)
+                .offset(y: currentCardScale * -130)
 
             Text("\(offset)")
         }
