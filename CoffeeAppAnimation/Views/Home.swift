50dce8063d4e4dd316ba21a0edd633310ee364e8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-11-28 19:37:14 -0500

    Added animated slider - title and price

--- CoffeeAppAnimation/Views/Home.swift
+++ CoffeeAppAnimation/Views/Home.swift
@@ -18,7 +18,7 @@ struct Home: View {
             let size = $0.size
             let cardSize = size.width
 
-            HeaderView()
+            HeaderView(items: coffees, index: currentIndex)
 
             LinearGradient(colors: [
                 .clear,
@@ -69,26 +69,65 @@ struct Home: View {
 }
 
 struct HeaderView: View {
+
+    private let coffees: [Coffee]
+    private let index: CGFloat
+
+    init(items coffees: [Coffee], index currentIndex: CGFloat) {
+        self.coffees = coffees
+        self.index = currentIndex
+    }
+
     var body: some View {
-        HStack {
-            Button {} label: {
-                Image(systemName: "chevron.left")
-                    .font(.title.bold())
-                    .foregroundColor(.black)
+        VStack {
+            HStack {
+                Button {} label: {
+                    Image(systemName: "chevron.left")
+                        .font(.title.bold())
+                        .foregroundColor(.black)
+                }
+
+                Spacer()
+
+                Button {} label: {
+                    Image(systemName: "cart.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.black)
+                }
             }
 
-            Spacer()
+            GeometryReader {
+                let size = $0.size
 
-            Button {} label: {
-                Image(systemName: "cart.fill")
-                    .resizable()
-                    .renderingMode(.template)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(.black)
+                HStack(spacing: 0) {
+                    ForEach(coffees) { coffee in
+                        VStack(spacing: 15) {
+                            Text(coffee.title)
+                                .font(.title.bold())
+                                .multilineTextAlignment(.center)
+
+                            Text(coffee.price)
+                                .font(.title)
+                        }
+                        .frame(width: size.width)
+                    }
+                }
+                .offset(x: index * -size.width)
+                .animation(.interactiveSpring(
+                    response: 0.6,
+                    dampingFraction: 0.8,
+                    blendDuration: 0.8), value: index)
             }
+            .padding(.top, 10)
+
+
+
         }
         .padding(25)
+
     }
 }
 
